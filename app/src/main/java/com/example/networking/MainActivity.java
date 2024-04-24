138e6bcc7d77523d27ef551856fe62d586bfd306
elyyz <a23eliax@student.his.se>
2024-04-24 11:30:31 +0200

    Ändrat många saker fram och tillbaka, tagit bort kod och lagt till kod osv.. Nu har jag kommit så långt att namnet på ett berg visas, "K2". Har fortfarande lite att jobba på.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -1,37 +1,59 @@
 package com.example.networking;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.List;
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
-
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private Gson gson;
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
-
     ArrayList<Mountain> items = new ArrayList<>();
-    RecyclerViewAdapter adapter;
-
-    RecyclerView recyclerView;
-
+    ArrayList<RecyclerViewItem> recyclerViewItems = new ArrayList<>();
+    private RecyclerViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.recyclerView);
-
-
-        new JsonFile(this, this).execute(JSON_FILE);
+        gson =new Gson();
+        for (int i=0;i< items.size();i++) {
+            Log.d("hej", items.get(i).toString());
+            recyclerViewItems.add(new RecyclerViewItem(items.get(i).toString()));
+        }
+        adapter = new RecyclerViewAdapter(this, recyclerViewItems, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(RecyclerViewItem item) {
+                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        RecyclerView view = findViewById(R.id.recyclerView);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(adapter);
+        //new JsonFile(this, this).execute(JSON_FILE);
+        new JsonTask(this).execute(JSON_URL);
     }
-
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     public void onPostExecute(String json) {
-        Log.d("MainActivity", json);
+        Log.d("Hej", json);
+        Type type = new TypeToken<List<Mountain>>() {}.getType();
+        List<Mountain> listOfMountains = gson.fromJson(json, type);
+        items.clear();
+        items.addAll(listOfMountains);
+        for (int i=0;i< items.size();i++) {
+            Log.d("Elinparsear", items.get(i).toString());
+            recyclerViewItems.add(new RecyclerViewItem(items.get(i).toString()));
+        }
+        adapter.notifyDataSetChanged();
     }
 
 }
--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,4 +1,22 @@
 package com.example.networking;
 
 public class Mountain {
+    private String name;
+    private String location;
+
+    public Mountain(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
 }
